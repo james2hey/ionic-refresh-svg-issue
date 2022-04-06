a36f043874a399a8a245ee23cf6720de046db63a
James Toohey <James.Toohey@vista.co>
2022-04-06 22:02:56 +1200

    Created repo to reproduce ionic refresher bug.

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonRefresher,
+    IonRefresherContent,
+    IonTitle,
+    IonToolbar,
+    RefresherEventDetail
+} from '@ionic/react';
 import './Tab1.css';
+import {useCallback, useState} from "react";
+import { ReactComponent as TestIcon } from '../test-icon.svg';
 
 const Tab1: React.FC = () => {
+    const [isRemounting, setIsRemounting] = useState(false);
+
+    const remountOccTimeout = useCallback(() => {
+        setIsRemounting(true);
+        window.setTimeout(() => {
+            setIsRemounting(false);
+        }, 100);
+    }, []);
+
+    const handleOnIonRefresh = useCallback(
+        (event: CustomEvent<RefresherEventDetail>) => {
+            window.setTimeout(() => {
+                event.detail.complete();
+                remountOccTimeout();
+            }, 1000);
+        },
+        [remountOccTimeout]
+    );
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +40,10 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+          <IonRefresher slot="fixed" onIonRefresh={handleOnIonRefresh}>
+              <IonRefresherContent />
+          </IonRefresher>
+          {isRemounting ? null : <TestIcon />}
       </IonContent>
     </IonPage>
   );
